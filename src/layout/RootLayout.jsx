38982ebf56a4b0670e6a96c88a729d3adb7b2a6e
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-09-12 01:02:47 +0500

    update

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/header/Header.jsx"
-import Footer from "../components/footer/footer.jsx"
+import Footer from "../components/footer/Footer.jsx"
 import { Outlet } from "react-router"
 function RootLayout() {
     return (
