b622c1578f4617f4530b86737308cc3130480385
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-12-05 17:03:31 +0500

    1

--- src/pages/grade/Grade.jsx
+++ src/pages/grade/Grade.jsx
@@ -11,7 +11,7 @@ function GradePage() {
   useEffect(() => {
     //  http://localhost:5000/grades/
     // https://four0-mak-server-3.onrender.com/grades/
-    fetch(`http://localhost:5000/grades/${gradeId}`)
+    fetch(`https://four0-mak-server-3.onrender.com/grades/${gradeId}`)
       .then((res) => res.json())
       .then((data) => setGradeData(data))
       .catch((err) => console.error("Fetch error:", err));
